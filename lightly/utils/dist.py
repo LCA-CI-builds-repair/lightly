faacfe5bb79e0ad9da4d5560515dbf6f90db0d53
ubuntu <ubuntu@ip-10-0-0-77>
2024-04-26 00:05:18 +0000

    test_lca_code_editing__code-editing-metrics-2aa59cb9eab490d8__id_66

--- lightly/utils/dist.py
+++ lightly/utils/dist.py
@@ -13,18 +13,24 @@ class GatherLayer(torch.autograd.Function):
     """
 
     @staticmethod
-    def forward(ctx, input: torch.Tensor) -> Tuple[torch.Tensor, ...]:
+import torch
+from torch.distributed import distributed_c10d as dist
+
+class Utils:
+    @staticmethod
+    def forward(cls, ctx, input: torch.Tensor) -> Tuple[torch.Tensor, ...]:
         ctx.save_for_backward(input)
-        output = [torch.empty_like(input) for _ in range(dist.get_world_size())]
-        dist.all_gather(output, input)
+        output = [torch.empty_like(input) for _ in range(torch.distributed.get_world_size())]
+        torch.distributed.all_gather(output, input)
         return tuple(output)
 
     @staticmethod
-    def backward(ctx, *grads: torch.Tensor) -> torch.Tensor:
-        (input,) = ctx.saved_tensors
-        grad_out = torch.empty_like(input)
-        grad_out[:] = grads[dist.get_rank()]
-        return grad_out
+import torch
+import torch.distributed as dist
+
+def rank() -> int:
+    """Returns the rank of the current process."""
+    return dist.get_rank() if dist.is_initialized() else 0
 
 
 def rank() -> int:
