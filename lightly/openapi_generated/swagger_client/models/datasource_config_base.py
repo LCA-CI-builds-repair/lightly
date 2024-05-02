7b406f705652e42c94d43906236288c916c2aad1
ubuntu <ubuntu@ip-10-0-0-215>
2024-05-02 11:45:41 +0000

    test_lca_code_editing__code-editing-metrics-cdfcab645d0a57a__id_62

--- lightly/openapi_generated/swagger_client/models/datasource_config_base.py
+++ lightly/openapi_generated/swagger_client/models/datasource_config_base.py
@@ -29,11 +29,10 @@ class DatasourceConfigBase(BaseModel):
     DatasourceConfigBase
     """
     id: Optional[constr(strict=True)] = Field(None, description="MongoDB ObjectId")
-    purpose: DatasourcePurpose = Field(...)
-    type: StrictStr = Field(...)
-    thumb_suffix: Optional[StrictStr] = Field(None, alias="thumbSuffix", description="the suffix of where to find the thumbnail image. If none is provided, the full image will be loaded where thumbnails would be loaded otherwise. - [filename]: represents the filename without the extension - [extension]: represents the files extension (e.g jpg, png, webp) ")
-    __properties = ["id", "purpose", "type", "thumbSuffix"]
-
+    purpose: DatasourcePurpose = Field(..., description="Add description here")
+    type: StrictStr = Field(..., description="Add description here")
+    thumb_suffix: Optional[StrictStr] = Field(None, alias="thumbSuffix", description="the suffix of where to find the thumbnail image. If none is provided, the full image will be loaded where thumbnails would be loaded otherwise. - [filename]: represents the filename without the extension - [extension]: represents the files extension (e.g jpg, png, webp)")
+    __properties__ = ["id", "purpose", "type", "thumbSuffix"]
     @validator('id')
     def id_validate_regular_expression(cls, value):
         """Validates the regular expression"""
