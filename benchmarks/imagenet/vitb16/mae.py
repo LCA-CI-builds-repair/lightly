6315988682b24c38519cce1ab314130073bb9864
ubuntu <ubuntu@ip-10-0-1-94>
2025-01-10 18:08:32 +0000

    waleko__claude-3-haiku_11f8__id_63

--- benchmarks/imagenet/vitb16/mae.py
+++ benchmarks/imagenet/vitb16/mae.py
@@ -4,7 +4,13 @@ import torch
 from pytorch_lightning import LightningModule
 from timm.models.vision_transformer import vit_base_patch16_224
 from torch import Tensor
-from torch.nn import MSELoss, Parameter
+from torch.nn import (
+    MSELoss,
+    Parameter,
+)
+from torch.optim import (
+    AdamW,
+)
 from torch.optim import AdamW
 
 from lightly.models import utils
