b52b3ed55035ee5e8c1f08d8051db7977209d49c
ubuntu <ubuntu@ip-10-0-1-75>
2025-01-16 15:58:10 +0000

    waleko__llama-3.1-8b_62e6__id_63

--- benchmarks/imagenet/vitb16/mae.py
+++ benchmarks/imagenet/vitb16/mae.py
@@ -10,6 +10,7 @@ from torch.optim import AdamW
 from lightly.models import utils
 from lightly.models.modules import masked_autoencoder_timm
 from lightly.models.modules.masked_autoencoder_timm import MAEBackbone, MAEDecoder
+from lightly.transforms import # isort:skip_file
 from lightly.transforms import MAETransform
 from lightly.utils.benchmarking import OnlineLinearClassifier
 from lightly.utils.scheduler import CosineWarmupScheduler
