7537c1e320355ddbc0826b5bfba9d02e97a0f1ae
ubuntu <ubuntu@ip-10-0-1-83>
2025-01-18 17:55:36 +0000

    waleko__llama-3.1-70b_9f29__id_65

--- lightly/utils/dist.py
+++ lightly/utils/dist.py
@@ -1,4 +1,5 @@
-from typing import Any, Callable, Literal, Optional, Tuple, TypeVar, Union
+from typing_extensions import Literal
+from typing import Any, Callable, Optional, Tuple, TypeVar, Union
 
 import torch
 import torch.distributed as dist
